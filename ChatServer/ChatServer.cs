32d8d2c9a05763a66ff7d637963ebdf8d855db74
hagaroffer <hagaroffer@gmail.com>
2025-01-17 20:41:31 +0200

    exception handling chatServer

--- ChatServer/ChatServer.cs
+++ ChatServer/ChatServer.cs
@@ -18,29 +18,39 @@ namespace ChatServer
             try
             {
                 InitVariables();
-
                 InitTcpListener();
-
                 InitQueueMessagesSenderThread();
 
                 Console.WriteLine("Chat server is running");
-
                 ListenToNewConnectionRequests();
-
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine(CreateExceptionMsg(ex, "InitServer - Socket Error"));
+                CleanupServer();
             }
             catch (Exception ex)
             {
-                return;
+                Console.WriteLine(CreateExceptionMsg(ex, "InitServer"));
+                CleanupServer();
             }
         }
 
         private void InitVariables()
         {
-            _clientsDictionary = new Dictionary<string, TcpClient>();
-            _clientsDictionaryLock = new object();
-            _messagesQueue = new Queue<ChatMessage>();
-            _messagesQueueLock = new object();
-            _eventWaitHandle = new AutoResetEvent(false);
+            try
+            {
+                _clientsDictionary = new Dictionary<string, TcpClient>();
+                _clientsDictionaryLock = new object();
+                _messagesQueue = new Queue<ChatMessage>();
+                _messagesQueueLock = new object();
+                _eventWaitHandle = new AutoResetEvent(false);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(CreateExceptionMsg(ex, "InitVariables"));
+                throw;
+            }
         }
 
         private void InitTcpListener()
@@ -50,20 +60,24 @@ namespace ChatServer
                 string ipAddress = Configurations.IpAddress;
                 int port = int.Parse(Configurations.Port);
 
-                _clientsDictionary = new Dictionary<string, TcpClient>();
-
                 _tcpListener = new TcpListener(IPAddress.Parse(ipAddress), port);
                 _tcpListener.Start();
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(CreateExceptionMsg(ex, "InitTcpListener - Invalid IP or Port"));
+                throw;
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine(CreateExceptionMsg(ex, "InitTcpListener - Socket Error"));
+                throw;
             }
             catch (Exception ex)
             {
                 Console.WriteLine(CreateExceptionMsg(ex, "InitTcpListener"));
-                if (_tcpListener != null)
-                {
-                    _tcpListener.Stop();
-                }
                 throw;
-            }           
+            }
         }
 
         private void InitQueueMessagesSenderThread()
@@ -71,6 +85,7 @@ namespace ChatServer
             try
             {
                 Thread senderThread = new Thread(new QueueMessagesSender(_messagesQueue, _messagesQueueLock, _eventWaitHandle, _clientsDictionary, _clientsDictionaryLock).HandleMessagesQueue);
+                senderThread.IsBackground = true;
                 senderThread.Start();
             }
             catch (Exception ex)
@@ -82,14 +97,45 @@ namespace ChatServer
 
         private void ListenToNewConnectionRequests()
         {
-            var connectionsListener = new NewConnectionsListener(_tcpListener, _clientsDictionary, _clientsDictionaryLock, _messagesQueue, _messagesQueueLock, _eventWaitHandle);
-            connectionsListener.ListenToConnectionRequests();
+            try
+            {
+                var connectionsListener = new NewConnectionsListener(
+                    _tcpListener,
+                    _clientsDictionary,
+                    _clientsDictionaryLock,
+                    _messagesQueue,
+                    _messagesQueueLock,
+                    _eventWaitHandle
+                );
+                connectionsListener.ListenToConnectionRequests();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(CreateExceptionMsg(ex, "ListenToNewConnectionRequests"));
+                throw;
+            }
+        }
+
+        private void CleanupServer()
+        {
+            try
+            {
+                Console.WriteLine("Cleaning up server resources...");
+                if (_tcpListener != null)
+                {
+                    _tcpListener.Stop();
+                    _tcpListener = null;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(CreateExceptionMsg(ex, "CleanupServer"));
+            }
         }
 
         private string CreateExceptionMsg(Exception ex, string methodName)
         {
-            string errorMsg = string.Format(@"Failure in {0}.{1}Error: {2}. {3}StackTrace: {4}", methodName, Environment.NewLine, ex.Message, Environment.NewLine, ex.StackTrace);
-            return errorMsg;
+            return $@"Failure in {methodName}.Error: {ex.Message}StackTrace: {ex.StackTrace}";
         }
     }
 }
